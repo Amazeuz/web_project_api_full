d9505c83376cc7c4fe8ced95d0e1fe3d38887b8e
Diuri <diuri.student@gmail.com>
2023-10-14 20:43:18 -0300

    remove crash-test

--- backend/app.js
+++ backend/app.js
@@ -24,12 +24,6 @@ app.options('*', cors());
 
 app.use(requestLogger)
 
-app.get('/crash-test', () => {
-  setTimeout(() => {
-    throw new Error('O servidor travará agora');
-  }, 0);
-});
-
 app.post('/signup', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
